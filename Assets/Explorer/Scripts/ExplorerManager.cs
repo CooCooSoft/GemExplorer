d27c575f17c153254a9a58fbef91ceeb2f5597a3
CooCooSoft <coocoosoft@outlook.com>
2018-11-25 15:44:26 +0800

    20181125

---
+++ Assets/Explorer/Scripts/ExplorerManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplorerManager : MonoBehaviour
+{
+    public float radius;
+    public GameObject[] Gems;
+
+    private Transform Camera;
+    // Use this for initialization
+    void Start()
+    {
+        Camera = GameObject.Find("MixedRealityCameraParent").transform;
+        for (int i = 0; i < Gems.Length; i++)
+        {
+            float z = radius * Mathf.Cos(2 * Mathf.PI / Gems.Length * i);
+            float x = radius * Mathf.Sin(2 * Mathf.PI / Gems.Length * i);
+            Instantiate(Gems[i], new Vector3(x, 0, z), Gems[i].transform.rotation, Camera);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Explorer/Scripts/GemController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class GemController : MonoBehaviour, IFocusable
+{
+    public float revolutionSpeed;
+    public float rotationSpeed;
+    public float cycle;
+    public float amplitude;
+    public GameObject Tag;
+
+    private float totalTime;
+    private Transform Camera;
+    private GameObject tag;
+
+    public void OnFocusEnter()
+    {
+        tag = Instantiate(Tag, transform);
+    }
+
+    public void OnFocusExit()
+    {
+        Destroy(tag);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        totalTime = Random.Range(0, cycle);
+        Camera = GameObject.Find("MixedRealityCameraParent").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        totalTime = (totalTime + Time.deltaTime) % cycle;
+        transform.Translate(Vector3.up * Time.deltaTime * amplitude * Mathf.Cos((2 * Mathf.PI / cycle) * totalTime));
+        transform.Rotate(Vector3.up, Time.deltaTime * rotationSpeed, Space.World);
+        transform.RotateAround(Camera.position, Vector3.up, Time.deltaTime * revolutionSpeed);
+    }
+}
